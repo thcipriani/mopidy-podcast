0a03c87d14d94453f9b977465a2aa706eb7e9032
Thomas Kemmer <tkemmer@computer.org>
2016-02-25 18:30:14 +0100

    Require Mopidy >= 1.1.1 for Extension classmethods.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'setuptools',
-        'Mopidy >= 1.1',
+        'Mopidy >= 1.1.1',
         'Pykka >= 1.1',
         'cachetools >= 1.0',
         'uritools >= 1.0'
